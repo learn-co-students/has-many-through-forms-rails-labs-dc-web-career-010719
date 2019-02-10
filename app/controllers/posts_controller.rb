a1843ab0f7c2588c914fed7fa5bfdd1017af68cb
canikwe <c.anikwe@gmail.com>
2019-02-10 07:57:09 -0500

    created nested forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments.build
+    @user = @comments.build_user
   end
 
   def index
@@ -16,9 +18,18 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
+  def update
+    @post = Post.find(params[:id])
+    byebug
+    @post.update(post_params)
+    redirect_to @post
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:id, :title, :content, category_ids:[],
+      categories_attributes: [:name], user_attributes: [:id, :username, :email],
+      comments_attributes: [:id, :content, :user_id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+c1 = Category.create(name: "Comedy")
+c2 = Category.create(name: "Drama")
+
+p1 = Post.create(title: "The Next Great American Hero", content: "To be revealed after I complete my research.")
+p2 = Post.create(title: "Far and Away", content: "TTo be or not to be that is the question.")
+
+p_c1 = PostCategory.create(post: p1, category: c1)
+p_c2 = PostCategory.create(post: p1, category: c2)
+p_c3 = PostCategory.create(post: p2, category: c2)
+
+ron = User.create(username: "Ron Weasley", email: "bigred94@gmail.com")
+bill = User.create(username: "Bill Weasley", email: "bigred87@gmail.com")
+gin = User.create(username: "Ginny Weasley", email: "bigred@gmail.com")
+
+
+com1 = Comment.create(content: "this post is awesome", user: ron, post: p1)
+com2 = Comment.create(content: "I learned so much", user: gin, post: p1)
+com3 = Comment.create(content: "truly inspired", user: bill, post: p1)
+com4 = Comment.create(content: "what's this have to do with potions?", user: ron, post: p2)
+com5 = Comment.create(content: "got here by accident. apologies", user: gin, post: p2)
+com6 = Comment.create(content: "this post is dank.", user: bill, post: p2)
